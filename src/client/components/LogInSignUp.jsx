f5300c474b865b0a1b905d6029d12ab8e0cf2063
Garron Ma <garronma@gmail.com>
2018-10-15 13:55:00 -0700

    New signup/login page

--- src/client/components/LogInSignUp.jsx
+++ src/client/components/LogInSignUp.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const LogInSignUp = (props) => {
+  const test = 24;
 
-}
+  return (
+    <div>
+      <h1>Log In/Sign Up</h1>
+      <p>{test}</p>
+    </div>
+  );
+};
 
 export default LogInSignUp;
