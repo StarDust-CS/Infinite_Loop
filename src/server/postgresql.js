7eb9ed0cfb1f8f44c7b3556f238237b059c1fcef
Garron Ma <garronma@gmail.com>
2018-10-15 17:50:34 -0700

    changed esql connection

--- src/server/postgresql.js
+++ src/server/postgresql.js
@@ -1,4 +1,4 @@
-const connection = 'postgres://gttmqzxg:h2Zkoz5a1R61uZT03gfGYTKfdOIZnURu@elmer.db.elephantsql.com:5432/gttmqzxg';
+const connection = 'postgres://uhwwflcv:rOFxI453H6EbPzA25RfSamRm_MQE__-S@elmer.db.elephantsql.com:5432/uhwwflcv';
 const pgp = require('pg-promise')(/*options*/);
 const db = pgp(connection);
 
