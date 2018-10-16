9709daa28b832535269729198e3ac6146370b43f
Garron Ma <garronma@gmail.com>
2018-10-15 17:51:16 -0700

    added POST /login

--- src/server/server.js
+++ src/server/server.js
@@ -1,34 +1,42 @@
 // initialize connection with PostgreSQL database
-const express = require ('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const userController = require('./controller/userController');
 const postController = require('./controller/postController');
-const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-app.post('/createuser',
+app.post('/login',
   userController.verifyUser,
-  userController.createUser, 
-  (req, res) => {
-    res.status(200).json(res.locals.data);
-  }
-)
-app.post('/createpost', 
-  postController.createPost, 
-  (req, res) => {
-    res.status(200).json(res.locals.data);
-});
-app.get('/home', 
-  postController.getPosts, 
-  (req, res) => {
-    res.status(200).json(res.locals.data);
-});
-app.patch('/status', 
-  postController.changeStatus, 
-  (req, res) => {
-    res.status(200).json(res.locals.data);
-});
+);
+
+// app.post('/createuser',
+//   userController.verifyUser,
+//   userController.createUser,
+//   (req, res) => {
+//     res.status(200).json(res.locals.data);
+//   }
+// )
+// app.post('/createpost',
+//   postController.createPost,
+//   (req, res) => {
+//     res.status(200).json(res.locals.data);
+//   });
+// app.get('/home',
+//   postController.getPosts,
+//   (req, res) => {
+//     res.status(200).json(res.locals.data);
+//   });
+// app.patch('/status',
+//   postController.changeStatus,
+//   (req, res) => {
+//     res.status(200).json(res.locals.data);
+//   });
+
+// below unchanged
+
 app.use(express.static(__dirname + '/../../dist'));
 
 app.use((err, req, res, next) => {
@@ -38,7 +46,7 @@ app.use((err, req, res, next) => {
 //this needs to be exported to userController.test in order to run tests
 module.exports = app.listen(3000, () => {
   console.log('Listening on port 3000...');
-} );
+});
 
 
 
