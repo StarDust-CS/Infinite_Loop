2e819a8ea43170fdfa47440954da5d0b22912e5b
Garron Ma <garronma@gmail.com>
2018-10-15 18:52:11 -0700

    working on create new user

--- src/server/controller/userController.js
+++ src/server/controller/userController.js
@@ -7,38 +7,39 @@ module.exports = {
     * @param { object } res response object
     * @param { function } next function to go to next middlewear 
     */
-  createUser(req, res, next) {
-    //check request has correct body format
-    if (Object.keys(req.body).length === 2 && req.body.name && req.body.role) {
-      //if role equals to user, create user 
-      if (req.body.role === 'student') {
-        db.one("INSERT INTO student(name) VALUES ($1) RETURNING *", req.body.name)
-          .then(data => {
-            res.locals.data = data;
-            return next();
-          })
-          .catch(err => {
-            console.log('ERROR: ', err)
-            return next(err);
-          });
-      } else {
-        // if not user create helper
-        db.one("INSERT INTO helper(name) VALUES ($1) RETURNING *", req.body.name)
-          .then(data => {
-            res.locals.data = data;
-            return next();
-          })
-          .catch(err => {
-            console.log('ERROR: ', err)
-            return next(err);
-          });
-      }
 
-    } else {
-      return res.status(400).send();
-    }
+  // createUser(req, res, next) {
+  //   //check request has correct body format
+  //   if (Object.keys(req.body).length === 2 && req.body.name && req.body.role) {
+  //     //if role equals to user, create user 
+  //     if (req.body.role === 'student') {
+  //       db.one("INSERT INTO student(name) VALUES ($1) RETURNING *", req.body.name)
+  //         .then(data => {
+  //           res.locals.data = data;
+  //           return next();
+  //         })
+  //         .catch(err => {
+  //           console.log('ERROR: ', err)
+  //           return next(err);
+  //         });
+  //     } else {
+  //       // if not user create helper
+  //       db.one("INSERT INTO helper(name) VALUES ($1) RETURNING *", req.body.name)
+  //         .then(data => {
+  //           res.locals.data = data;
+  //           return next();
+  //         })
+  //         .catch(err => {
+  //           console.log('ERROR: ', err)
+  //           return next(err);
+  //         });
+  //     }
 
-  },
+  //   } else {
+  //     return res.status(400).send();
+  //   }
+
+  // },
   /**
    * Checks to see if user is in the database, if not calls createUser()
    * @param {Object} req - request object
@@ -48,11 +49,12 @@ module.exports = {
 
   verifyUser(req, res, next) {
     const { email, password } = req.body;
-    db.query('SELECT * FROM users WHERE email = $1', [email])
-      // .then(data => data.json())
-      .then(data => console.log(data)) //data[0]
-      .then(data => res.status(200).send(`${data} found successfully`))
-      .catch(err => console.log(err));
+    db.any('SELECT * FROM users WHERE email = $1', [email])
+
+      // results return as array of object (unique email, so 0)
+      .then(data => data[0])
+      .then(data => res.status(200).send(data))
+      .catch(err => console.error(err));
   },
 
   // verifyUser(req, res, next) {
